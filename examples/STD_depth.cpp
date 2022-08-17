a018f856618b4a011b8e6c130518d5584d9a2fb4
zhangli <li.zhang@edge-perception.com>
2022-08-17 16:25:16 +0800

    add example

--- examples/STD_depth.cpp
+++ examples/STD_depth.cpp
@@ -18,6 +18,12 @@ volatile bool g_is_start = false;
 volatile int g_fps = 0;
 volatile unsigned long g_last_time = 0;
 volatile int g_frame_count = 0;
+
+std::thread fpsThread;
+
+#include <stdlib.h>
+#include <stdio.h>
+
 void calculate_framerate()
 {
 
@@ -35,7 +41,27 @@ void calculate_framerate()
 	}
 }
 
+void print_device_info(sy3::device *dev)
+{
+	sy3::sy3_error e;
+	printf("\nUsing device 0, an %s\n", sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_NAME, e));
+	printf("    Serial number: %s\n", sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_SERIAL_NUMBER, e));
+	printf("    Firmware version: %s\n\n", sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_FIRMWARE_VERSION, e));
+}
+void print_support_format(sy3::device *dev, sy3::sy3_error &e)
+{
 
+	std::vector<sy3::sy3_stream> support_stream = dev->get_support_stream(e);
+	for (int i = 0; i < support_stream.size(); i++)
+	{
+		printf("support stream:%s \n", sy3_stream_to_string(support_stream[i]));
+		std::vector<sy3::sy3_format> support_format = dev->get_support_format(support_stream[i], e);
+		for (int j = 0; j < support_format.size(); j++)
+		{
+			printf("\t\t support format:%d x %d \n", support_format[j].width, support_format[j].height);
+		}
+	}
+}
 
 int  index_time = 1;
 void show_ir_frame(sy3::ir_frame *frame, const char* name)
@@ -152,6 +178,9 @@ int main(int argc, char **argv)
 	}
 
 	printf("    Firmware version: %s   sn: %s   %s\n\n", sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_FIRMWARE_VERSION, e), sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_SERIAL_NUMBER, e), sy3::sy3_get_device_info(dev, sy3::SY3_CAMERA_INFO_RECOMMENDED_FIRMWARE_VERSION, e));
+	print_support_format(dev, e);
+	print_device_info(dev);
+
 
 	sy3::pipeline *pline = sy3::sy3_create_pipeline(ctx, e);
 
@@ -167,43 +196,13 @@ int main(int argc, char **argv)
 
 	std::thread fpsThread = std::thread(calculate_framerate);
 
+	uint16_t filter_value = 0;
+
 	while (!quit)
 	{
 		cv::waitKey(1);
 
 
-		if (nIndex >= 200000) {
-
-			switch (switch_flag) {
-
-			case 0: {
-
-				printf("=============> 	pline->stop(e) \n");
-				pline->stop(e);
-				cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_DEPTH, 640, 480, e);
-				cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_IR, 640, 480, e);
-				printf("=============> 	pline->start(cfg, e) \n");
-				pline->start(cfg, e);
-				printf("=============>  switch 640x480   %d \n", index_time++);
-				switch_flag = !switch_flag;
-				nIndex = 1;
-			}break;
-
-			case 1:
-			{
-				pline->stop(e);
-				cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_DEPTH, 320, 240, e);
-				cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_IR, 320, 240, e);
-				pline->start(cfg, e);
-				printf("=============>  switch 320x240   %d \n", index_time++);
-				switch_flag = !switch_flag;
-				nIndex = 0;
-			}break;
-
-			default:
-				break;
-			}
-		}
 
 		sy3::frameset *frameset = pline->wait_for_frames(SY3_DEFAULT_TIMEOUT, e);
 		sy3::depth_frame *depth_frame = frameset->get_depth_frame();
@@ -222,6 +221,106 @@ int main(int argc, char **argv)
 
 		delete frameset;
 		nIndex++;
+		switch (cv::waitKey(30))
+		{
+
+		case 'a':
+		{
+
+			pline->stop(e);
+			cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_DEPTH, 640, 480, e);
+			cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_IR, 640, 480, e);
+			pline->start(cfg, e);
+			switch_flag = !switch_flag;
+			nIndex = 1;
+			printf("%s  %d   switch 640x480\n", __FILE__, __LINE__);
+		}
+		break;
+
+		case 'b':
+		{
+			pline->stop(e);
+			cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_DEPTH, 320, 240, e);
+			cfg->enable_stream(sy3::sy3_stream::SY3_STREAM_IR, 320, 240, e);
+			pline->start(cfg, e);
+			switch_flag = !switch_flag;
+			nIndex = 0;
+			printf("%s  %d   switch 320x240\n", __FILE__, __LINE__);
+		}
+		break;
+
+		case 'c':
+		{
+
+			uint16_t value;
+			dev->get_sensor(e)->get_option(sy3::sy3_option::SY3_OPTION_DEPTH_IMAGE_FILTER, value, e);
+			printf("SY3_OPTION_DEPTH_IMAGE_FILTER:%d \n", value);
+		}
+		break;
+
+		case 'd':
+		{
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_DEPTH_IMAGE_FILTER, filter_value, e);
+			filter_value = !filter_value;
+		}
+		break;
+
+		case '0':
+		{
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_TOF_IMAGE_FLIP, 0, e);
+		}
+		break;
+
+		case '1':
+		{
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_TOF_IMAGE_FLIP, 1, e);
+		}
+		break;
+
+		case '2':
+		{
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_TOF_IMAGE_MIRROR, 0, e);
+		}
+		break;
+
+		case '3':
+		{
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_TOF_IMAGE_MIRROR, 1, e);
+		}
+		break;
+
+		case 'e': {
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_EXPOSURE, 300, e);
+		} break;
+
+		case 'f': {
+			dev->get_sensor(e)->set_option(sy3::sy3_option::SY3_OPTION_EXPOSURE, 1000, e);
+		} break;
+
+		case 'g': {
+
+			uint16_t exposure = 0;
+			dev->get_sensor(e)->get_option(sy3::sy3_option::SY3_OPTION_EXPOSURE, exposure, e);
+
+		} break;
+
+		case 'q': {
+
+			g_is_start = false;
+			pline->stop(e);
+			delete ctx;
+			cv::destroyAllWindows();
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			quit = true;
+			break;
+
+		}break;
+
+		default:
+
+			break;
+		}
+
 	}
 	system("pause");
 
