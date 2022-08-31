8018f4f0136430b9aa59f66753eadc00555ba4de
zhangli <li.zhang@edge-perception.com>
2022-08-31 10:02:12 +0800

    no message

---
+++ include/libsynexens3/context.h
@@ -0,0 +1,43 @@
+/**
+ * @file context.h
+ * @author HMS
+ * @brief
+ * @version 0.0.4
+ * @date 2022-04-24
+ *
+ * Copyright(c) 2022 synexens.Co.Ltd. All rights reserved
+ *
+ */
+#ifndef LIBSYNEXENS3_CONTEXT_H
+#define LIBSYNEXENS3_CONTEXT_H
+
+#include <memory>
+#include "macros.h"
+#include "device-info.h"
+
+namespace SY3_NAMESPACE
+{
+
+	class SY3_EXPORT context
+	{
+
+	public:
+
+		/**
+		 * \brief デバイス情報を取得する
+		 * \return
+		 */
+		virtual const device_info *get_device_info(sy3_error &error) const = 0;
+
+		/**
+		 * \brief デバイスを探す
+		 * \return
+		 */
+		virtual device *query_device(sy3_error &error) const = 0;
+
+		virtual ~context() = default;
+	};
+
+};
+
+#endif
